e9e16ae15e4bf79b1c37b3111813abf635deb184
Sayan Maity <89021746+Sayan-Maity@users.noreply.github.com>
2022-12-27 06:26:32 +0530

    modified the styling

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    // extend: {
+    //   backgroundColor: {
+    //     'dark-green': '#06bbbc',
+    //   },
+    // },
+    // colors: {
+    //   main: "#151923",
+    // },
+  },
+  plugins: [],
+};
