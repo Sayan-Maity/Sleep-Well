035db957bbafe0e76d9a9e96c084cb10fe443fac
Sayan Maity <89021746+Sayan-Maity@users.noreply.github.com>
2022-12-27 06:28:03 +0530

    added google api key

---
+++ src/apis/youtube.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const KEY = process.env.REACT_APP_KEY;
+
+export default axios.create({
+  baseURL: "https://www.googleapis.com/youtube/v3",
+  params: {
+    part: "snippet",
+    type: "video",
+    key: KEY,
+  },
+});
